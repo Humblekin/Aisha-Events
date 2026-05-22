657139213b7f68bc63c6782751f4438afdd649f2
SaaS Builder <builder@saas.app>
2026-05-22 12:04:29 +0000

    feat: dynamic copyright year using Date().getFullYear()

--- src/components/public/Footer.jsx
+++ src/components/public/Footer.jsx
@@ -53,7 +53,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="footer-bottom">
-          <span>2026 Aisha Events Centre. All rights reserved.</span>
+          <span>{new Date().getFullYear()} Aisha Events Centre. All rights reserved.</span>
           <span>Crafted with elegance in Tamale, Ghana</span>
         </div>
       </div>
